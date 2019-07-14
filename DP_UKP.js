a5c003f1e94193e84dfdbd69bb2e045234af7571
Mesṭafa Kamal <em_ben_ramdane@esi.dz>
2019-07-14 13:45:16 +0100

    Update DP_UKP.js

--- DP_UKP.js
+++ DP_UKP.js
@@ -1,14 +1,5 @@
 /*
-Apparemment regarde dynamic programming de UKP
-Pour le DP normalement tu vas trouver sur google l'explication pour le cas UKP
-
-*/
-
-/*
-Dans le cas de UKP, on peut prendre une ou plusieurs istance d'un objet. Afin de ustifier le problème par la programmation dynamique, nous définissons un sous problème propre à un sac à dos de capacté i compris entre 0 et W. Nous utilisons un vecteur dp[W+1] de sorte que 
-dp[i] contient la valeur maximale pouvant être atteinte en utilisant tous les articles et un sac à dos de capacité i. 
-A chaque itération de i, on parcours les objets et on choisit de prendre celui dont la valeur est supérieure à la valeur actuelle, et cela sans dépasser la capacité i du sac.
-La solution au problème global est celle où i atteint W.
+UKP with dynamic programming
 
 */
 
